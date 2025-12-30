bad3ae79253af1b7154255596b7d0689b7a27fd5
DESKTOP-SF0IA8J\rosli <roscarcius@gmail.com>
2025-12-30 23:25:10 +0100

    fix: correct right thumb cluster key positions to match physical layout

--- keymap-editor/app.js
+++ keymap-editor/app.js
@@ -352,12 +352,12 @@ class KeymapEditor {
             47: { x: 310, y: 190 },
             
             // Fila 4 - Thumb cluster (índices 54-59, 6 teclas)
-            54: { x: 50, y: 265 },   // SPACE
-            55: { x: 110, y: 255 },  // ENTER
-            56: { x: 170, y: 250 },  // mo 2
-            57: { x: 230, y: 250 },  // RALT
-            58: { x: 290, y: 250 },  // RCTRL
-            59: { x: 350, y: 250 }   // DEL
+            54: { x: 350, y: 250 },  // SPACE (derecha)
+            55: { x: 290, y: 250 },  // ENTER
+            56: { x: 230, y: 250 },  // mo 2
+            57: { x: 170, y: 250 },  // RALT
+            58: { x: 110, y: 255 },  // RCTRL
+            59: { x: 50, y: 265 }    // DEL (izquierda)
         };
         
         return positions[index] || { x: 0, y: 0 };
